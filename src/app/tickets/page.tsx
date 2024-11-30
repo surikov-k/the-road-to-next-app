5079d17f92fb3b4661bba71390cba274e4902cd0
Konstantin Surikov <19418260+surikov-k@users.noreply.github.com>
2024-11-30 22:42:03 +0300

    eslint and prettier

--- src/app/tickets/page.tsx
+++ src/app/tickets/page.tsx
@@ -1,24 +1,42 @@
-import Link from 'next/link';
-import {initialTickets} from '@/data';
-import { ticketPath } from '@/paths';
+import Link from "next/link";
+
+import { initialTickets } from "@/data";
+import { ticketPath } from "@/paths";
 
 const TICKET_ICONS = {
   DONE: "✅",
   IN_PROGRESS: "⏳",
   OPEN: "📂",
-}
+};
 
 export default function TicketsPage() {
   return (
-    <div>
-      <h2 className="text-2xl">Tickets</h2>
-      {initialTickets.map((ticket) => (
-        <div key={ticket.id}>
-          <div>{TICKET_ICONS[ticket.status]}</div>
-          <h2 className="text-lg">{ticket.title}</h2>
-          <Link className="underline text-sm" href={ticketPath(ticket.id.toString())} >Go to ticket</Link>
-        </div>
-      ))}
+    <div className='flex flex-1 flex-col gap-y-8'>
+      <div>
+        <h2 className='text-3xl font-bold tracking-tight'>Tickets</h2>
+        <p className='text-muted-foreground text-sm'>
+          All your tickets at one place
+        </p>
+      </div>
+
+      <div className='flex flex-1 flex-col items-center gap-y-4'>
+        {initialTickets.map((ticket) => (
+          <div
+            key={ticket.id}
+            className='w-full max-w-[420px] rounded border border-slate-600 p-4'
+          >
+            <div>{TICKET_ICONS[ticket.status]}</div>
+            <h3 className='truncate text-lg font-semibold'>{ticket.title}</h3>
+            <p className='truncate text-sm text-slate-500'>{ticket.content}</p>
+            <Link
+              className='text-sm underline'
+              href={ticketPath(ticket.id.toString())}
+            >
+              Go to ticket
+            </Link>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
--- src/data.ts
+++ src/data.ts
@@ -2,7 +2,7 @@ export const initialTickets = [
   {
     id: 1,
     title: "Ticket 1",
-    content: "This is the ticket 1",
+    content: "Damned! Damned! Damned silence. He refuses to bow, he refuses to drink. Majesty! Did you know we assigned one of our *best* pain technicians -- pain technicians, they used to be called torturers, ever since they got organized it's 'pain technicians' -- Why are you here? One of our very best torturers, I felt certain he would break him. Two hours he worked, not a sound. I said: 'Give me a cry, rin-tintsy(?). Give me a shout, a whimper, a scream.' Silence! So, I got to it myself. You can't leave these things to others, they never get it right! And well, you can see for yourself. If I didn't know better, I would say he was a mute. Silence! I'm beginning to understand what you're going through with this G'Kar. How you put up with this at all, I have no idea. He was a small burden." ,
     status: "DONE" as const
   },
   {
