0ea1533fd092cb07d17406924520bfe4e07ddc53
Konstantin Surikov <19418260+surikov-k@users.noreply.github.com>
2024-11-30 17:27:54 +0300

    Path constants

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,11 @@
 import Link from 'next/link';
+import { ticketsPath } from '@/paths';
 
 export default function HomePage() {
   return (
     <div>
       <h2 className="text-4xl">Home Page</h2>
-      <Link href="/tickets" className="underline">Go to tickets</Link>
+      <Link href={ticketsPath()} className="underline">Go to tickets</Link>
     </div>
   );
 }
--- src/app/tickets/page.tsx
+++ src/app/tickets/page.tsx
@@ -1,5 +1,6 @@
 import Link from 'next/link';
 import {initialTickets} from '@/data';
+import { ticketPath } from '@/paths';
 
 export default function TicketsPage() {
   return (
@@ -8,7 +9,7 @@ export default function TicketsPage() {
       {initialTickets.map((ticket) => (
         <div key={ticket.id}>
           <h2 className="text-lg">{ticket.title}</h2>
-          <Link className="underline text-sm" href={`/tickets/${ticket.id}`} >Go to ticket</Link>
+          <Link className="underline text-sm" href={ticketPath(ticket.id.toString())} >Go to ticket</Link>
         </div>
       ))}
     </div>
