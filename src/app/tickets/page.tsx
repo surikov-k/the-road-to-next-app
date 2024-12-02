96c76735416a80b033b5b8dae7cb81220cc6c363
Konstantin Surikov <19418260+surikov-k@users.noreply.github.com>
2024-12-02 17:31:53 +0300

    Card Compact (Configuration over composition)

--- src/app/tickets/page.tsx
+++ src/app/tickets/page.tsx
@@ -1,16 +1,10 @@
 import { Suspense } from "react";
 import { ErrorBoundary } from "react-error-boundary";
 
+import CardCompact from "@/components/card-compact";
 import Heading from "@/components/Heading";
 import Placeholder from "@/components/placeholder";
 import Spinner from "@/components/spinner";
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card";
 import TicketCreateForm from "@/features/ticket/componets/ticket-create-form";
 import TicketList from "@/features/ticket/componets/ticket-list";
 
@@ -19,16 +13,12 @@ export default function TicketsPage() {
     <div className='flex flex-1 flex-col gap-y-8'>
       <Heading title='Tickets' description='All your tickets at one place' />
 
-      <Card className='w-full max-w-[420px] self-center'>
-        <CardHeader>
-          <CardTitle>Create Ticket</CardTitle>
-          <CardDescription>Create a new ticket</CardDescription>
-        </CardHeader>
-
-        <CardContent>
-          <TicketCreateForm />
-        </CardContent>
-      </Card>
+      <CardCompact
+        title='Create Ticket'
+        description='Create a new ticket'
+        content={<TicketCreateForm />}
+        className='w-full max-w-[420px] self-center'
+      />
 
       <ErrorBoundary fallback={<Placeholder label='Failed to load tickets' />}>
         <Suspense fallback={<Spinner />}>
---
+++ src/components/card-compact.tsx
@@ -0,0 +1,36 @@
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+
+type CardCompactProps = {
+  title: string;
+  description: string;
+  content: React.ReactNode;
+  className?: string;
+  footer?: React.ReactNode;
+};
+
+export default function CardCompact({
+  title,
+  description,
+  content,
+  className,
+  footer,
+}: CardCompactProps) {
+  return (
+    <Card className={className}>
+      <CardHeader>
+        <CardTitle>{title}</CardTitle>
+        <CardDescription>{description}</CardDescription>
+      </CardHeader>
+
+      <CardContent>{content}</CardContent>
+      {footer && <CardFooter>{footer}</CardFooter>}
+    </Card>
+  );
+}
