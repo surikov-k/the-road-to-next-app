d8ab930fb40926a9e73f178ddf89b084e1f7bda6
Konstantin Surikov <19418260+surikov-k@users.noreply.github.com>
2024-12-02 09:40:46 +0300

    Error boundary

--- src/app/tickets/page.tsx
+++ src/app/tickets/page.tsx
@@ -1,6 +1,8 @@
 import { Suspense } from "react";
+import { ErrorBoundary } from "react-error-boundary";
 
 import Heading from "@/components/Heading";
+import Placeholder from "@/components/placeholder";
 import Spinner from "@/components/spinner";
 import TicketList from "@/features/ticket/componets/ticket-list";
 
@@ -8,9 +10,12 @@ export default function TicketsPage() {
   return (
     <div className='flex flex-1 flex-col gap-y-8'>
       <Heading title='Tickets' description='All your tickets at one place' />
-      <Suspense fallback={<Spinner />}>
-        <TicketList />
-      </Suspense>
+
+      <ErrorBoundary fallback={<Placeholder label='Failed to load tickets' />}>
+        <Suspense fallback={<Spinner />}>
+          <TicketList />
+        </Suspense>
+      </ErrorBoundary>
     </div>
   );
 }
