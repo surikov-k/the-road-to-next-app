fcf35904a1b1ee77a661ff646be727580163335c
Konstantin Surikov <19418260+surikov-k@users.noreply.github.com>
2024-12-03 15:27:53 +0300

    Actions Callbacks

--- src/features/ticket/componets/ticket-upsert-form.tsx
+++ src/features/ticket/componets/ticket-upsert-form.tsx
@@ -3,6 +3,7 @@
 import { Ticket } from "@prisma/client";
 import { useActionState } from "react";
 
+import { useActionFeedback } from "@/components/form/hooks/use-action-feedback";
 import SubmitButton from "@/components/form/submit-button";
 import { EMPTY_ACTION_STATE } from "@/components/form/utils/to-action-state";
 import { Input } from "@/components/ui/input";
@@ -19,6 +20,16 @@ export default function TicketUpsertForm({ ticket }: TicketUpsertFormProps) {
     upsertTicket.bind(null, ticket?.id),
     EMPTY_ACTION_STATE
   );
+
+  useActionFeedback(actionState, {
+    onSuccess: ({ actionState }) => {
+      console.log(actionState.message);
+    },
+    onError: ({ actionState }) => {
+      console.log(actionState.message);
+    },
+  });
+
   return (
     <div>
       <form action={action} className='flex flex-col gap-y-2'>
@@ -52,7 +63,6 @@ export default function TicketUpsertForm({ ticket }: TicketUpsertFormProps) {
         </div>
 
         <SubmitButton label={ticket ? "Update" : "Create"} />
-        <span className='text-sm text-red-600'>{actionState.message}</span>
       </form>
     </div>
   );
