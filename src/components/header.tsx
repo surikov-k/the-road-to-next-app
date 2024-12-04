8a51c898fa505af449ff59d40b8b22d3717e8bc9
Konstantin Surikov <19418260+surikov-k@users.noreply.github.com>
2024-12-04 22:36:05 +0300

    Paths for authentication

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -3,9 +3,32 @@ import Link from "next/link";
 
 import ThemeSwitcher from "@/components/theme/theme-switcher";
 import { buttonVariants } from "@/components/ui/button";
-import { homePath, ticketsPath } from "@/paths";
+import { homePath, signInPath, signUpPath, ticketsPath } from "@/paths";
 
 export default function Header() {
+  const navItems = (
+    <>
+      <Link
+        href={ticketsPath()}
+        className={buttonVariants({ variant: "outline" })}
+      >
+        Tickets
+      </Link>
+      <Link
+        href={signUpPath()}
+        className={buttonVariants({ variant: "outline" })}
+      >
+        Sign up
+      </Link>
+      <Link
+        href={signInPath()}
+        className={buttonVariants({ variant: "outline" })}
+      >
+        Sign in
+      </Link>
+    </>
+  );
+
   return (
     <nav className='supports-backdrop-blur:bg-white/60 fixed left-0 right-0 top-0 z-20 flex w-full justify-between border-b bg-background/95 px-5 py-2.5 backdrop-blur'>
       <div>
@@ -19,12 +42,7 @@ export default function Header() {
       </div>
       <div className='flex items-center gap-1'>
         <ThemeSwitcher />
-        <Link
-          href={ticketsPath()}
-          className={buttonVariants({ variant: "outline" })}
-        >
-          Tickets
-        </Link>
+        {navItems}
       </div>
     </nav>
   );
--- src/paths.ts
+++ src/paths.ts
@@ -3,3 +3,6 @@ export const ticketsPath = () => "/tickets";
 export const ticketPath = (ticketId: string) => `${ticketsPath()}/${ticketId}`;
 export const editTicketPath = (ticketId: string) =>
   `${ticketPath(ticketId)}/edit`;
+export const signUpPath = () => "/sign-up";
+export const signInPath = () => "/sign-in";
+export const passwordForgotPath = () => "/password-forgot";
