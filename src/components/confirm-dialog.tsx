1a5c703251b373eff9729f37820af20d33c4773c
Konstantin Surikov <19418260+surikov-k@users.noreply.github.com>
2024-12-04 15:17:17 +0300

    Confirm dialog

--- src/components/confirm-dialog.tsx
+++ src/components/confirm-dialog.tsx
@@ -1,12 +1,50 @@
 import { ReactElement } from "react";
 
-import { ActionState } from "@/components/form/utils/to-action-state";
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "@/components/ui/alert-dialog";
+import { Button } from "@/components/ui/button";
 
 interface ConfirmDialogProps {
-  action: () => Promise<ActionState>;
+  action: () => Promise<void>;
+  description?: string;
+  title?: string;
   trigger: ReactElement;
 }
 
-export default function ConfirmDialog({ action, trigger }: ConfirmDialogProps) {
-  return <form>{trigger}</form>;
+export default function ConfirmDialog({
+  action,
+  description = "This action cannot be undone. Make sure you understand the consequences.",
+  title = "Are you absolute sure?",
+  trigger,
+}: ConfirmDialogProps) {
+  return (
+    <AlertDialog>
+      <AlertDialogTrigger asChild>{trigger}</AlertDialogTrigger>
+      <AlertDialogContent>
+        <AlertDialogHeader>
+          <AlertDialogTitle>{title}</AlertDialogTitle>
+          <AlertDialogDescription>{description}</AlertDialogDescription>
+        </AlertDialogHeader>
+        <AlertDialogFooter>
+          <AlertDialogCancel>Cancel</AlertDialogCancel>
+          <AlertDialogAction asChild>
+            <form action={action} className='px-0'>
+              <Button type='submit' variant='destructive'>
+                Confirm
+              </Button>
+            </form>
+          </AlertDialogAction>
+        </AlertDialogFooter>
+      </AlertDialogContent>
+    </AlertDialog>
+  );
 }
