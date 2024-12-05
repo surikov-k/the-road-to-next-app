b442d9b8a7c020a26972f897f6fef8a1d4a6c5d4
Konstantin Surikov <19418260+surikov-k@users.noreply.github.com>
2024-12-06 00:02:04 +0300

    Breadcrumbs

--- src/app/tickets/[ticketId]/edit/page.tsx
+++ src/app/tickets/[ticketId]/edit/page.tsx
@@ -1,10 +1,13 @@
 import { notFound } from "next/navigation";
 
+import Breadcrumbs from "@/components/breadcrumbs";
 import CardCompact from "@/components/card-compact";
+import { Separator } from "@/components/ui/separator";
 import { getAuth } from "@/features/auth/queries/get-auth";
 import isOwner from "@/features/auth/utils/is-owner";
 import TicketUpsertForm from "@/features/ticket/components/ticket-upsert-form";
 import { getTicket } from "@/features/ticket/queries/get-ticket";
+import { homePath, ticketPath } from "@/paths";
 
 interface EditTicketPageProps {
   params: Promise<{
@@ -25,13 +28,24 @@ export default async function EditTicketPage({ params }: EditTicketPageProps) {
   }
 
   return (
-    <div className='flex flex-1 flex-col items-center justify-center'>
-      <CardCompact
-        title='Edit Ticket'
-        description='Edit a ticket'
-        content={<TicketUpsertForm ticket={ticket} />}
-        className='w-full max-w-[420px] animate-fade-in-from-top'
+    <div className='flex flex-1 flex-col gap-y-8'>
+      <Breadcrumbs
+        breadcrumbs={[
+          { title: "Tickets", href: homePath() },
+          { title: ticket.title, href: ticketPath(ticket.id) },
+          { title: "Edit" },
+        ]}
       />
+
+      <Separator />
+      <div className='flex flex-1 flex-col items-center justify-center'>
+        <CardCompact
+          title='Edit Ticket'
+          description='Edit a ticket'
+          content={<TicketUpsertForm ticket={ticket} />}
+          className='w-full max-w-[420px] animate-fade-in-from-top'
+        />
+      </div>
     </div>
   );
 }
--- src/app/tickets/[ticketId]/page.tsx
+++ src/app/tickets/[ticketId]/page.tsx
@@ -1,10 +1,12 @@
 import Link from "next/link";
 
+import Breadcrumbs from "@/components/breadcrumbs";
 import Placeholder from "@/components/placeholder";
 import { Button } from "@/components/ui/button";
+import { Separator } from "@/components/ui/separator";
 import TicketItem from "@/features/ticket/components/ticket-item";
 import { getTicket } from "@/features/ticket/queries/get-ticket";
-import { ticketsPath } from "@/paths";
+import { homePath, ticketsPath } from "@/paths";
 
 type TicketPageProps = {
   params: Promise<{
@@ -29,8 +31,19 @@ export default async function TicketPage({ params }: TicketPageProps) {
     );
   }
   return (
-    <div className={"flex animate-fade-in-from-top justify-center"}>
-      <TicketItem ticket={ticket} hasDetail />
+    <div className='flex flex-1 flex-col gap-y-8'>
+      <Breadcrumbs
+        breadcrumbs={[
+          { title: "Tickets", href: homePath() },
+          { title: ticket.title },
+        ]}
+      />
+
+      <Separator />
+
+      <div className={"flex animate-fade-in-from-top justify-center"}>
+        <TicketItem ticket={ticket} hasDetail />
+      </div>
     </div>
   );
 }
