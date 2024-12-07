d67c7a5ccfcec14727f91ce73df0fccbd9513966
Konstantin Surikov <19418260+surikov-k@users.noreply.github.com>
2024-12-07 20:46:15 +0300

    Route Params in Route Handler [B]

--- src/components/pagination.tsx
+++ src/components/pagination.tsx
@@ -84,6 +84,7 @@ export default function Pagination({
         <SelectValue />
       </SelectTrigger>
       <SelectContent>
+        <SelectItem value='2'>2</SelectItem>
         <SelectItem value='5'>5</SelectItem>
         <SelectItem value='10'>10</SelectItem>
         <SelectItem value='25'>25</SelectItem>
