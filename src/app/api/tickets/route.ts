f8bcde1ef2b60d5686dec815e6f9480fb5440978
Konstantin Surikov <19418260+surikov-k@users.noreply.github.com>
2024-12-07 21:06:43 +0300

    Search Params in Route Handler

--- src/app/api/tickets/route.ts
+++ src/app/api/tickets/route.ts
@@ -4,21 +4,12 @@ import { searchParamsCache } from "@/features/ticket/search-params";
 export async function GET(request: Request) {
   const { searchParams } = new URL(request.url);
 
-  const untypedSearchParams = Object.fromEntries(searchParams);
-
-  const typedSearchParams = searchParamsCache.parse(untypedSearchParams);
-
-  console.log(untypedSearchParams);
-  console.log(typedSearchParams);
+  const typedSearchParams = searchParamsCache.parse(
+    Object.fromEntries(searchParams)
+  );
 
   const { list, metadata } = await getTickets(
-    {
-      search: "",
-      page: 0,
-      size: 10,
-      sortKey: "createdAt",
-      sortValue: "desc",
-    },
+    Promise.resolve(typedSearchParams),
     undefined
   );
   return Response.json({ list, metadata });
